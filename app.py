d731b669adcbbf7aac43149d2a6d8346e09381e6
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2022-11-27 21:07:25 +0000

    added api_name

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ with gr.Blocks(css=css) as block:
         
         gr.HTML(article)
 
-    submit_btn.click(fn=inference, inputs=[input_image,mode_input,flavor_input], outputs=[output_text, share_button, community_icon, loading_icon])
+    submit_btn.click(fn=inference, inputs=[input_image,mode_input,flavor_input], outputs=[output_text, share_button, community_icon, loading_icon], api_name="clipi2")
     share_button.click(None, [], [], _js=share_js)
     
 block.queue(max_size=32,concurrency_count=20).launch(show_api=False)
